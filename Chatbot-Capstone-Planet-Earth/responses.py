73ab3c2b2dc70bd06b13f04dccb7091c07ff079a
Bschake <99057740+bschake@users.noreply.github.com>
2022-07-04 09:20:51 -0600

    Create responses.py
    This file contains a list of responses for the chatbot.

---
+++ Chatbot-Capstone-Planet-Earth/responses.py
@@ -0,0 +1,31 @@
+response_a = "The {} lives in the Arctic, and is a fierce hunter.  It's liver contains high quantities of Vitamin A, and climate change is affecting its habitat as the ice melts."
+response_b = "The {} has the longest neck, and also a weird tongue."
+response_c = "{} are pets, but they are not super interactive.  People often consider them aloof, and they are famous on the internet."
+response_d = "{} are commonly eaten on Thanksgiving in the United States, and even though they are birds they can't fly."
+response_e = "Everyone loves {} because they are friendly and protective.  They are domesticated, and can live in your house as a pet."
+response_f = "The {} lives in a tree, and is very slow.  They have been referred to as lazy."
+response_g = "The {} lives in the water, but it breathes air.  They are the largest animals on Earth. It is a marine mammal."
+response_h = "The {} is extinct, a meteor wiped its species out.  But, people have found their bones and learned about what they looked like, and we made Jurassic Park in their honor."
+
+
+blank_spot = "animal"
+
+responses = [
+    response_a,
+    response_b,
+    response_c,
+    response_d,
+    response_e,
+    response_f,
+    response_g,
+    response_h
+    ]
+
+#a = polar bear
+#b = giraffe
+#c = cat
+#d = turkey
+#e = dog
+#f = sloth
+#g = whale
+#h = dinosaur
